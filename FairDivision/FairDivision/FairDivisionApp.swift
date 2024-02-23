bc36788cfc5e0aafa9d3067b881a2d193a2b9950
srikarp <air4bball@gmail.com>
2024-02-23 15:26:33 -0600

    Created Xcode project

---
+++ FairDivision/FairDivision/FairDivisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FairDivisionApp.swift
+//  FairDivision
+//
+//  Created by Srikar on 2/23/24.
+//
+
+import SwiftUI
+
+@main
+struct FairDivisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
